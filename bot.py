8ef58d1adfbbd7ca63a2ce1ae9149f21ff484c00
n141421356n-star <n141421356n@gmail.com>
2026-07-08 13:26:55 +0900

    Add files via upload

--- bot.py
+++ bot.py
@@ -28,7 +28,37 @@ async def map(ctx):
         "images/e_district.png",
         "images/storm_point.png"
     ]
+@bot.command()
+async def next(ctx):
+    MAPS = [
+        "ワールズエッジ",
+        "イーディス",
+        "ストームポイント"
+    ]
 
+    IMAGE_PATHS = [
+        "images/worlds_edge.png",
+        "images/e_district.png",
+        "images/storm_point.png"
+    ]
+
+    JST = datetime.timezone(datetime.timedelta(hours=9))
+
+    start_time = datetime.datetime(2026, 7, 4, 2, 0, 0, tzinfo=JST)
+    now = datetime.datetime.now(JST)
+
+    interval = 4.5 * 60 * 60
+
+    elapsed = (now - start_time).total_seconds()
+
+    index = int(elapsed // interval) % len(MAPS)
+
+    next_index = (index + 1) % len(MAPS)
+
+    await ctx.send(
+        f"次のマップは：{MAPS[next_index]}",
+        file=discord.File(IMAGE_PATHS[next_index])
+    )
     JST = datetime.timezone(datetime.timedelta(hours=9))
 
     start_time = datetime.datetime(2026, 7, 4, 2, 0, 0, tzinfo=JST)
