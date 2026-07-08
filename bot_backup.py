f38fabbcc073cfed7fe6225b726df0f383682861
n141421356n-star <n141421356n@gmail.com>
2026-07-08 13:09:27 +0900

    Add files via upload

---
+++ bot_backup.py
@@ -0,0 +1,39 @@
+import discord
+import datetime
+from discord.ext import commands
+import os
+
+MAP_IMAGES = {
+    "ワールズエッジ": "images/worlds_edge.png",
+    "イーディス": "images/e_district.png",
+    "ストームポイント": "images/storm_point.png"
+}
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"ログイン成功: {bot.user}")
+
+@bot.command()
+async def test(ctx):
+    await ctx.send("動いてるよ！")
+
+@bot.command()
+async def map(ctx):
+    JST = datetime.timezone(datetime.timedelta(hours=9))
+
+    start_time = datetime.datetime(2026, 7, 4, 2, 0, 0, tzinfo=JST)
+    now = datetime.datetime.now(JST)
+
+    interval = 4.5 * 60 * 60
+    elapsed = (now - start_time).total_seconds()
+    index = int(elapsed // interval) % len(MAPS)
+
+    await ctx.send(f"現在のマップは：{MAPS[index]}")
+    
+
+bot.run(os.getenv("DISCORD_TOKEN"))
