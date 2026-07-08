81f68ee82797ba2e96c7555aa39f328569774779
n141421356n-star <n141421356n@gmail.com>
2026-07-08 13:22:45 +0900

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,39 +1,35 @@
 import discord
 import datetime
 from discord.ext import commands
-import os
-
-MAPS = [
-    "ワールズエッジ",
-    "イーディス",
-    "ストームポイント"
-]
-
-intents = discord.Intents.default()
-intents.message_content = True
-
-bot = commands.Bot(command_prefix="!", intents=intents)
-
-@bot.event
-async def on_ready():
-    print(f"ログイン成功: {bot.user}")
-
-@bot.command()
-async def test(ctx):
-    await ctx.send("動いてるよ！")
 
 @bot.command()
 async def map(ctx):
+    MAPS = [
+        "ワールズエッジ",
+        "イーディス",
+        "ストームポイント"
+    ]
+
+    IMAGE_PATHS = [
+        "images/worlds_edge.png",
+        "images/e_district.png",
+        "images/storm_point.png"
+    ]
+
     JST = datetime.timezone(datetime.timedelta(hours=9))
 
     start_time = datetime.datetime(2026, 7, 4, 2, 0, 0, tzinfo=JST)
     now = datetime.datetime.now(JST)
 
     interval = 4.5 * 60 * 60
+
     elapsed = (now - start_time).total_seconds()
+
     index = int(elapsed // interval) % len(MAPS)
 
-    await ctx.send(f"現在のマップは：{MAPS[index]}")
-    
-
+    await ctx.send(
+        f"現在のマップは：{MAPS[index]}",
+        file=discord.File(IMAGE_PATHS[index])
+    )
+import os
 bot.run(os.getenv("DISCORD_TOKEN"))
